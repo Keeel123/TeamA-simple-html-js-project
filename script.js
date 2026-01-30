45c95a77ce290f8e10994a6d0e5cfe3c12076f4f
Keeel123 <kristhanlope@gmail.com>
2026-01-30 22:07:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 function showMessage(){
 	document.getElementById("output").innerText = "Hello from JavaScript! Love Joy Hope";
 }
-
-function changeColor(){
-    document.body.style.backgroundColor = "lightblue";
-}
